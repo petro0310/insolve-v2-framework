ea501b2952f9f467dcaa2a5336ac38428070ebf5
Damir-Petro Spahich <146043120+petro0310@users.noreply.github.com>
2024-01-29 16:42:18 +0900

    [Backend Url changed]

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: 'https://tf2double.com',
+  url: 'https://luckyrust.gg',
   // url: 'http://192.248.183.186',
   port: 443, // port that serves live updates
   appid: 252490 // tf2 = 440, csgo = 730, 252490 = rust
